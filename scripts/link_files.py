29e58b7048f82f1c95f02b37545e83a02ca652a4
Tien Vo <tien.a.vo@proton.me>
2023-07-26 09:28:38 -0600

    Finished loading routines

--- scripts/link_files.py
+++ scripts/link_files.py
@@ -14,16 +14,7 @@ for root, dirs, files in os.walk(lib.h5_dir):
         h5f[f"{path}/{os.path.splitext(f)[0]}"] = h5.ExternalLink(
             f"{root}/{f}", "/")
 
-#probes = range(1, 5)
-#instruments = [
-#    "fgm", "edp", "ion-fpi-moms", "elc-fpi-moms", "ion-feeps", "elc-feeps"
-#]
-#for probe in probes:
-#    for instrument in instruments:
-#        for interval in range(N_intervals := read_num_intervals()):
-#            h5f[f"mms{probe}/{instrument}/interval_{interval}"] = h5.ExternalLink(
-#                lib.data_dir / f"h5/mms{probe}/{instrument}/interval_{interval}.h5", "/")
-#
+
 #for interval in range(N_intervals):
 #    h5f[f"postprocess/interval_{interval}"] = h5.ExternalLink(
 #        lib.postprocess_dir / f"interval_{interval}.h5", "/")
