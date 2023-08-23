258ab7bc86dc1b376f997820dba806458059b781
Tien Vo <tien.a.vo@proton.me>
2023-08-23 02:52:33 -0600

    Working on EDP

--- src/mms_survey/load/edp.py
+++ src/mms_survey/load/edp.py
@@ -1,19 +1,20 @@
+__all__ = ["LoadEDP"]
+
 import os
 import warnings
 
 from cdflib.xarray import cdf_to_xarray
 
-from mms_survey.utils.directory import zarr_store
+from mms_survey.utils.directory import zarr_store, zarr_compressor
 
-from .base import BaseDownload
-from .download import download_file
+from .base import BaseLoader
+from .download import download
 from .utils import fix_epoch_metadata
 
 warnings.filterwarnings("ignore")
 
 
-class DownloadEDP(BaseDownload):
-
+class LoadEDP(BaseLoader):
     def __init__(
         self,
         start_date: str = "2017-07-26",
@@ -31,24 +32,39 @@ class DownloadEDP(BaseDownload):
             data_rate="fast" if data_rate == "srvy" else data_rate,
             data_type=data_type,
             data_level=data_level,
+            query_type="science",
         )
 
-    @staticmethod
-    def process(file: str):
-        temp_file = download_file(file)
-        if temp_file is None:
-            return f"Issue processing {file}. File was not processed"
-
-        ds = cdf_to_xarray(temp_file, to_datetime=True, fillval_to_nan=True)
-        probe, instrument, data_rate, level, data_type, tid, _ = file.split("_")
+    def process(self, file: str):
+        # Extract some metadata from file name
+        (
+            probe, instrument, data_rate, level, data_type, tid, _
+        ) = file.split("_")
         pfx = f"{probe}_{instrument}"
         sfx = f"{data_rate}_{level}"
-        ds = fix_epoch_metadata(ds, vars=[f"{pfx}_epoch_{sfx}",])
+        if instrument != "edp":
+            return f"File {file} is not in EDP dataset!"
+
+        # Download file into temporary file
+        url = f"{self.server}/download/{self.query_type}?file={file}"
+        temp_file = download(url)
+        if temp_file is None:
+            return f"Issue encountered! {file} was not processed!"
+
+        # Fix dataset dimension & metadata
+        ds = fix_epoch_metadata(
+            cdf_to_xarray(temp_file, to_datetime=True, fillval_to_nan=True),
+            vars=[f"{pfx}_epoch_{sfx}"],
+        )
 
         if data_type == "dce":
             vars = {
-                f"{pfx}_{var}_{sfx}": var for var in [
-                    "dce_gse", "dce_par_epar", "dce_err", "bitmask",
+                f"{pfx}_{var}_{sfx}": var
+                for var in [
+                    "dce_gse",
+                    "dce_par_epar",
+                    "dce_err",
+                    "bitmask",
                 ]
             }
         elif data_type == "scpot":
@@ -69,5 +85,5 @@ class DownloadEDP(BaseDownload):
 
 
 if __name__ == "__main__":
-    d = DownloadEDP(data_type="dce")
+    d = LoadEDP(data_type="dce")
     d.download()
