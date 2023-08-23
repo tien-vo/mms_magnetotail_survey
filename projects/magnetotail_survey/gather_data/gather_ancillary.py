54ee21064ca5d4d29c9c988350aa70a287bd65e8
Tien Vo <tien.a.vo@proton.me>
2023-08-22 20:49:59 -0600

    Added ancillary module and reorganized scripts

--- projects/magnetotail_survey/gather_data/gather_ancillary.py
+++ projects/magnetotail_survey/gather_data/gather_ancillary.py
@@ -1,5 +1,4 @@
 from mms_survey.load.ancillary import DownloadANCIL
 
-for year in range(2017, 2021):
-    d = DownloadANCIL(start_date=f"{year}-04-01", end_date=f"{year}-11-01")
-    d.download(dry_run=True)
+d = DownloadANCIL(start_date="2017-01-01", end_date="2021-01-01")
+d.download(dry_run=False)
